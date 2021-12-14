62b1bdc1de43a863eb7cd933b400b36e66926d18
Arianna Johnson <ariannajohnson588@gmail.com>
2021-12-14 15:05:42 -0500

    some mock data

---
+++ src/data/products.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        id: 1,
+        name: "blueberry",
+        price: 6
+    },
+    {
+        id: 2,
+        name: "strawberry",
+        price: 5
+    },
+    {
+        id: 3,
+        name: "banana",
+        price: 2
+    },
+    {
+        id: 4,
+        name: "cake",
+        price: 25
+    }
+];
+
+export default products;
